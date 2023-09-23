2e79b10d93cdaa8c75e9f557b4be35d8dbee0fbf
Onajite Kelvin <onajiteokeh@gmail.com>
2023-09-23 01:06:16 +0100

    update

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+        <h1 className="text-4xl text-center text-rose-500">Home Page</h1>
+        <p>Test</p>
+    </div>
+  )
+}
+
+
+export default Home
